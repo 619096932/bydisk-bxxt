260fe7b92bb4b4859a2821bb1b181f2958e3e5f5
批发靓鸡仔 <lan894734188@gmail.com>
2016-06-15 12:59:53 +0800

    Update conn.php

--- conn.php
+++ conn.php
@@ -3,6 +3,6 @@ if(is_file($_SERVER['DOCUMENT_ROOT'].'/360safe/360webscan.php')){
     require_once($_SERVER['DOCUMENT_ROOT'].'/360safe/360webscan.php');
 } // 注意文件路径
 date_default_timezone_set("Asia/Shanghai");
- $conn = new mysqli('127.0.0.1:3306','root','tset','bxxt') or die("哎呀出错了啦");  
- $conn->query("SET NAMES utf8");
+$conn = new mysqli('address:3306','username','password','database') or die("哎呀出错了啦");  
+$conn->query("SET NAMES utf8");
 ?>  
